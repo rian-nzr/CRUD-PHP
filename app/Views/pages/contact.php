e12ce525a6fec866ca4fd726d4550225f31ef6f6
kaimju <90521244+rian-nzr@users.noreply.github.com>
2023-08-17 00:50:28 +0700

    Update contact.php

--- app/Views/pages/contact.php
+++ app/Views/pages/contact.php
@@ -1,21 +1,21 @@
-<?= $this->extend('layout/template'); ?>
-
-<?= $this->section('content'); ?>
-
-<div class=" ">
-    <p>contact</p>
-
-    <?php foreach ($alamat as $a) : ?>
-        <ul>
-            <li><?= $a['tipe']; ?></li>
-            <li><?= $a['alamat']; ?></li>
-            <li><?= $a['kota']; ?></li>
-        </ul>
-
-    <?php endforeach; ?>
-
-</div>
-
-
-
-<?= $this->endSection(); ?>
+    <?= $this->extend('layout/template'); ?>
+    
+    <?= $this->section('content'); ?>
+    
+    <div class=" ">
+        <p>contact</p>
+    
+        <?php foreach ($alamat as $a) : ?>
+            <ul>
+                <li><?= $a['tipe']; ?></li>
+                <li><?= $a['alamat']; ?></li>
+                <li><?= $a['kota']; ?></li>
+            </ul>
+    
+        <?php endforeach; ?>
+    
+    </div>
+    
+    
+    
+    <?= $this->endSection(); ?>
